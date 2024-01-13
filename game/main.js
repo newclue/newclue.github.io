889bd11c7d981dbbd423d1495d55010fec6fe76d
newclue <80074563+newclue@users.noreply.github.com>
2024-01-13 16:27:45 -0700

    Update main.js

--- game/main.js
+++ game/main.js
@@ -90,8 +90,8 @@ function App() {
       var w, h;
       var s = Math.tan(fovInDegrees * (Math.PI / 180.0) / 2.0) * near;
       //
-      w = Math.sqrt(aspect);
-      h = 1 / w;
+      w = s * Math.sqrt(aspect);
+      h = s * (1 / w);
       //
       var p = mat4.create();
       mat4.frustum(p, -w, w, -h, h, near, far);
