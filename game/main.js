116d95f4a139b822c04b916aba4c91ae74d2e2d7
newclue <80074563+newclue@users.noreply.github.com>
2024-01-13 14:45:39 -0700

    Update main.js

--- game/main.js
+++ game/main.js
@@ -2,6 +2,82 @@
 Tutorial: https://github.com/google/filament/blob/main/web/docs/tutorial_triangle.md
 */
 
+function main() {
+  // Convenient aliases
+  var VertexAttribute = Filament.VertexAttribute;
+  var AttributeType = Filament.VertexBuffer$AttributeType;
+  var PrimitiveType = Filament.RenderableManager$PrimitiveType;
+  var IndexType = Filament.IndexBuffer$IndexType;
+  var Fov = Filament.Camera$Fov;
+  var LightType = Filament.LightManager$Type;
+  var Projection = Filament.Camera$Projection;
+  
+  var canvas = document.getElementsByTagName('canvas')[0];
+  
+  var engine = Filament.Engine.create(canvas);
+  var scene = engine.createScene();
+  
+  ////
+  var triangle = Filament.EntityManager.get().create();
+  scene.addEntity(triangle);
+
+  var TRIANGLE_POSITIONS = new Float32Array([
+    1, 0,
+    Math.cos(Math.PI * 2 / 3), Math.sin(Math.PI * 2 / 3),
+    Math.cos(Math.PI * 4 / 3), Math.sin(Math.PI * 4 / 3),
+  ]);
+  var TRIANGLE_COLORS = new Uint32Array([0xffff0000, 0xff00ff00, 0xff0000ff]);
+  
+  var vb = Filament.VertexBuffer.Builder()
+    .vertexCount(3)
+    .bufferCount(2)
+    .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT2, 0, 8)
+    .attribute(VertexAttribute.COLOR, 1, AttributeType.UBYTE4, 0, 4)
+    .normalized(VertexAttribute.COLOR)
+    .build(engine);
+  vb.setBufferAt(engine, 0, TRIANGLE_POSITIONS);
+  vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
+  
+  var ib = Filament.IndexBuffer.Builder()
+    .indexCount(3)
+    .bufferType(IndexType.USHORT)
+    .build(engine);
+  ib.setBuffer(engine, new Uint16Array([0, 1, 2]));
+
+  var mat = engine.createMaterial('triangle.filamat');
+  var matinst = mat.getDefaultInstance();
+  Filament.RenderableManager.Builder(1)
+    .boundingBox({ center: [-1, -1, -1], halfExtent: [1, 1, 1] })
+    .material(0, matinst)
+    .geometry(0, PrimitiveType.TRIANGLES, vb, ib)
+    .build(engine, triangle);
+  ////
+  
+  var swapChain = engine.createSwapChain();
+  var renderer = engine.createRenderer();
+  var camera = engine.createCamera(Filament.EntityManager.get().create());
+  var view = engine.createView();
+  view.setCamera(camera);
+  view.setScene(scene);
+  resize();
+  window.addEventListener('resize', resize);
+  window.requestAnimationFrame(render);
+  
+  function render() {
+    renderer.render(swapChain, view);
+    window.requestAnimationFrame(render);
+  }
+
+  function resize() {
+    var dpr = window.devicePixelRatio;
+    var width = canvas.width = canvas.clientWidth * dpr;
+    var height = canvas.height = canvas.clientHeight * dpr;
+    view.setViewport([0, 0, width, height]);
+    var aspect = width / height;
+    camera.setProjection(Projection.ORTHO, -aspect, aspect, -1, 1, 0, 1);
+  }
+}
+
 if (typeof glMatrix !== 'undefined') {
   // This is a hack because glMatrix modules are not globally available on load. They are all within `glMatrix`.
   // The Filament glMatrix extensions expect global availability.
@@ -23,78 +99,3 @@ function loadBufferExtensions() {
 }
 
 Filament.init([ 'triangle.filamat', ], main);
-
-function main() {
-  
-  var canvas = document.getElementsByTagName('canvas')[0];
-  
-  var engine = Filament.Engine.create(canvas);
-  var scene = engine.createScene();
-  
-  var triangle = Filament.EntityManager.get().create();
-  scene.addEntity(triangle);
-
-  ////
-  var TRIANGLE_POSITIONS = new Float32Array([
-    1, 0,
-    Math.cos(Math.PI * 2 / 3), Math.sin(Math.PI * 2 / 3),
-    Math.cos(Math.PI * 4 / 3), Math.sin(Math.PI * 4 / 3),
-  ]);
-  var TRIANGLE_COLORS = new Uint32Array([0xffff0000, 0xff00ff00, 0xff0000ff]);
-  
-  var VertexAttribute = Filament.VertexAttribute;
-  var AttributeType = Filament.VertexBuffer$AttributeType;
-  
-  var vb = Filament.VertexBuffer.Builder()
-    .vertexCount(3)
-    .bufferCount(2)
-    .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT2, 0, 8)
-    .attribute(VertexAttribute.COLOR, 1, AttributeType.UBYTE4, 0, 4)
-    .normalized(VertexAttribute.COLOR)
-    .build(engine);
-  vb.setBufferAt(engine, 0, TRIANGLE_POSITIONS);
-  vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
-  
-  var ib = Filament.IndexBuffer.Builder()
-    .indexCount(3)
-    .bufferType(Filament.IndexBuffer$IndexType.USHORT)
-    .build(engine);
-  ib.setBuffer(engine, new Uint16Array([0, 1, 2]));
-  ////
-
-  var mat = engine.createMaterial('triangle.filamat');
-  var matinst = mat.getDefaultInstance();
-  Filament.RenderableManager.Builder(1)
-    .boundingBox({ center: [-1, -1, -1], halfExtent: [1, 1, 1] })
-    .material(0, matinst)
-    .geometry(0, Filament.RenderableManager$PrimitiveType.TRIANGLES, vb, ib)
-    .build(engine, triangle);
-
-  var swapChain = engine.createSwapChain();
-  var renderer = engine.createRenderer();
-  var camera = engine.createCamera(Filament.EntityManager.get().create());
-  var view = engine.createView();
-  view.setCamera(camera);
-  view.setScene(scene);
-  
-  renderer.setClearOptions({clearColor: [0.0, 0.1, 0.2, 1.0], clear: true});
-
-  resize();
-  window.addEventListener('resize', resize);
-  window.requestAnimationFrame(render);
-  
-  function render() {
-    renderer.render(swapChain, view);
-    window.requestAnimationFrame(render);
-  }
-
-  function resize() {
-    var dpr = window.devicePixelRatio;
-    var width = canvas.width = canvas.clientWidth * dpr;
-    var height = canvas.height = canvas.clientHeight * dpr;
-    view.setViewport([0, 0, width, height]);
-    var aspect = width / height;
-    var Projection = Filament.Camera$Projection;
-    camera.setProjection(Projection.ORTHO, -aspect, aspect, -1, 1, 0, 1);
-  }
-}
