446de2e9153eb5f54645f804daab88693cda6266
newclue <80074563+newclue@users.noreply.github.com>
2024-01-26 15:13:48 -0700

    Update main.js

--- game/main.js
+++ game/main.js
@@ -109,6 +109,7 @@ function App() {
     var b = m1;
     out[0] = a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
     out[1] = a[0] * b[3] + a[1] * b[4] + a[2] * b[5];
+    return out;
   }
   /**
    * @param {vec3} out - resulting hexagonal coordinate
@@ -121,6 +122,7 @@ function App() {
     out[0] = a[0] * b[0] + a[1] * b[1];
     out[1] = a[0] * b[2] + a[1] * b[3];
     out[2] = a[0] * b[4] + a[1] * b[5];
+    return out;
   }
   ////
 
