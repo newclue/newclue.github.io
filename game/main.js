6c5a72f25b2e8ee590e1779374cac939cf867ab0
newclue <80074563+newclue@users.noreply.github.com>
2024-01-26 17:29:51 -0700

    Update main.js

--- game/main.js
+++ game/main.js
@@ -42,10 +42,10 @@ function App() {
   vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
   
   var ib = Filament.IndexBuffer.Builder()
-    .indexCount(7)
+    .indexCount(9)
     .bufferType(IndexType.USHORT)
     .build(engine);
-  ib.setBuffer(engine, new Uint16Array([0, 1, 2, 3, 4, 5, 0]));
+  ib.setBuffer(engine, new Uint16Array([0, 1, 2, 2, 3, 4, 4, 5, 0]));
 
   var mat = engine.createMaterial('triangle.filamat');
   var matinst = mat.getDefaultInstance();
@@ -174,7 +174,7 @@ function App() {
   }
 
   spawnGrid();
-  var n = 1 / 32;
+  var n = 1 / 24;
   camera.setScaling([ n, n, ]);
   ////
 
